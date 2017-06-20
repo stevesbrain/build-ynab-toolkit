cae1a995724069c3f4557c11adff7464addf8ccc
stevesbrain <stevesbrain@users.noreply.github.com>
2017-06-20 10:54:35 +0930

    Initial commit of build files

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:8.1.2-alpine
+WORKDIR /root
+RUN apk update; apk add git python2 bash rsync openssl; rm -rf /var/cache/apk/*
+RUN git clone https://github.com/toolkit-for-ynab/toolkit-for-ynab
+WORKDIR /root/toolkit-for-ynab
+RUN git checkout 9ca8ac1
+RUN npm install
+RUN ./build
+CMD [ "/bin/ash" ]
---
+++ get-files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Please wait while your image is built"
+IMAGEID=$(sudo docker build ./ | grep -i success | cut -d' ' -f3)
+sudo docker run -v /root/toolkit-for-ynab $IMAGEID /bin/sh
+CONTAINERID=$(sudo docker ps -l | sed -n 2p | cut -d' ' -f1)
+echo "Copying the build now"
+sudo docker cp $CONTAINERID:/root/toolkit-for-ynab/output .
+echo "Removing created container + image"
+sudo docker rm $CONTAINERID
+sudo docker rmi $IMAGEID
+echo "Done"
